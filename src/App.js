5ed4d56c27a9c8e4a9f87c93debedc68907e5e75
Abdul Hannan Malik <hannan@cplusoft.com>
2024-10-21 18:10:27 +0500

    Setup Deployment Modified Gitignore file Created Vervel Package

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+import Pusher from "pusher-js";
 
 function App() {
+  const apiURL = process.env.NESTJS_APP_BACKEND || 'http://localhost:8000/api/'
+  const [username, setUsername] = useState("username");
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+  
+  useEffect(() => {
+    // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true;
+    
+    const pusher = new Pusher('ab3de070f05de881bc75', {
+      cluster: 'ap2'
+    });
+    
+    let allMessages = []
+    const channel = pusher.subscribe('chat');
+    channel.bind('message', function(data) {
+      allMessages.push(data)
+      setMessages(allMessages)
+    });
+  }, []);
+
+  const submit = async e => {
+    e.preventDefault()
+
+    await fetch(apiURL + 'messages', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        username,
+        message
+      })
+    })
+    setMessage('')
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-body-tertiary">
+        <div
+          href="/"
+          class="d-flex align-items-center flex-shrink-0 p-3 link-body-emphasis text-decoration-none border-bottom"
         >
-          Learn React
-        </a>
-      </header>
+          <input
+            class="fs-5 fw-semibold"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+
+        <div class="list-group list-group-flush border-bottom scrollarea">
+          {messages.map((message) => {
+            return (
+              <div key={message.username} class="list-group-item list-group-item-action py-3 lh-sm">
+                <div class="d-flex w-100 align-items-center justify-content-between">
+                  <strong class="mb-1">{message.username}</strong>
+                </div>
+                <div class="col-10 mb-1 small">{message.message}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <form onSubmit={(e) => submit(e)}>
+        <input
+          className="form-control"
+          placeholder="Write a message!"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </form>
     </div>
   );
 }
